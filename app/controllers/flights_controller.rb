f852ad812ae8ef0b35fdd0fb2dfcc3d5024a5d52
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-27 08:11:44 +0100

    create flight model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @flights = Flight.search(params)
     @airports = Airport.pluck(:code, :id).sort.uniq
     @departures = Flight.departures
     @passengers = (1..4)
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,4 +5,12 @@ class Flight < ActiveRecord::Base
   def self.departures
     pluck(:departure).map { |departure| departure.strftime("%d/%m/%Y") }.sort.uniq
   end
+
+  def self.search(params)
+    flights = Flight.all
+    flights = flights.where(from_airport_id: params[:from]) if params[:from].present?
+    flights = flights.where(to_airport_id: params[:to]) if params[:to].present?
+    flights = flights.where(departure: params[:departure]) if params[:departure].present?
+    flights
+  end
 end
